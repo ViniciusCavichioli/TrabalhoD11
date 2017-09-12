d101632ff2cb1882e31b0fe6cd8620aada34b8a8
Vinicius Cavichioli <vinicavi@hotmail.com>
2017-09-11 21:59:04 -0300

    Consultas

--- trabalho/eventos_tr/views.py
+++ trabalho/eventos_tr/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from eventos_tr.models import *
+
+def listaEvento(request):
+    retorno = "<h1>Eventos</h1>"
+    lista = Evento.objects.all()
+    for evento in lista:
+        retorno += '</br> Nome do Evento: {} </br>'.format(evento.nome)
+    return HttpResponse(retorno)
+
+def get_evento_byID(request, id):
+    retorno = "<h1>Evento</h1>"
+    evento = Evento.objects.get(pk = id)
+    retorno = retorno + '</br> Nome do Evento: {}</br>'.format(evento.nome)
+    return HttpResponse(retorno)
 
 # Create your views here.
--- trabalho/trabalho/urls.py
+++ trabalho/trabalho/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from eventos_tr.views import listaEvento
+from eventos_tr.views import get_evento_byID
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^eventos/$', listaEvento, name = 'listaEvento'),
+    url(r'^eventos/ ([0-9]{1}) /', get_evento_byID),
+
 ]
